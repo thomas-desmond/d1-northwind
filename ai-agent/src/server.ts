e9e48b7565920d82f5602fa115d3d1f26c7745c4
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2025-08-08 21:22:29 -0700

    Use openai model

--- ai-agent/src/server.ts
+++ ai-agent/src/server.ts
@@ -13,13 +13,9 @@ import {
 import { openai } from "@ai-sdk/openai";
 import { processToolCalls } from "./utils";
 import { tools, executions } from "./tools";
-import { env } from "cloudflare:workers";
+// import { env } from "cloudflare:workers";
 
 const model = openai("gpt-4o-2024-11-20");
-// const workersai = createWorkersAI({ binding: env.AI });
-// const model = workersai("@cf/meta/llama-3.3-70b-instruct-fp8-fast");
-
-
 // Cloudflare AI Gateway
 // const openai = createOpenAI({
 //   apiKey: env.OPENAI_API_KEY,
